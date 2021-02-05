0d6a1939a148a8239a5b243a8b1dc228135dfcde
Jonatan Eriksson <jonatan-eriksson@users.noreply.github.com>
2021-02-05 14:34:16 +0100

    Add url function to create url with search params

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
-window.addEventListener("DOMContentLoaded", onPageLoad);
+"use strict";
 
+// api urls
+const weatherUrl = new URL("https://api.openweathermap.org/data/2.5/weather");
+const foursquareUrl = new URL("https://api.foursquare.com/v2/venues/explore");
+
+// När sidan laddats kör eventet
+window.addEventListener("DOMContentLoaded", onPageLoad);
 function onPageLoad() {
   const cityForm = document.querySelector("#city-form");
   cityForm.addEventListener("submit", onCitySearch);
@@ -7,6 +13,18 @@ function onPageLoad() {
 
 function onCitySearch(e) {
   // const citySearch = document.querySelector("#city-search").value;
-  alert("City submitted " + this.city.value);
+  const weatherKey = config.openweatherKey;
+  const city = this.city.value;
+  const url = generateUrl(weatherUrl, { q: city, units: "metric", appid: weatherKey });
+  console.log(url);
+
   e.preventDefault();
+}
+
+function generateUrl(baseUrl, searchArgs) {
+  let url = new URL(baseUrl);
+  for (let key in searchArgs) {
+    url.searchParams.set(key, searchArgs[key]);
+  }
+  return url;
 }
